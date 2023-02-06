9d2d5ade030c8880d0e3e957e7e0f31cdae36ad0
DarioGiusino <dario.giusino@gmail.com>
2023-02-06 16:20:46 +0100

    generated censure word program

---
+++ badword.php
@@ -0,0 +1,42 @@
+<?php
+$text = $_GET['original'];
+$censure = $_GET['censured'];
+$censured_text = str_replace($censure, '***', $text);
+$censured_text_length = strlen($censured_text);
+
+
+var_dump($text);
+var_dump($censure);
+
+
+if(str_contains($text, $censure)){
+    $result = "Il testo censurato è '$censured_text' ed è lungo $censured_text_length caratteri";
+} else {
+    $result = 'Il testo non contiene la parola da censurare';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords2</title>
+</head>
+<body style="background-color: #0d1117; color:#f0f6fc;">
+<h1>Testo originale</h1>
+<p>Il testo originale era '<?= $text ?>'' ed era lungo <?= strlen($text)?> caratteri</p>
+
+<hr>
+
+<h1>Testo censurato</h1>
+<p><?= $result ?></p>
+
+<hr>
+
+<h1>Parola censurata</h1>
+<p><?= $censure ?></p>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
-<body>
-    <h1>ciao come sta?</h1>
+<body style="background-color: #0d1117; color:#f0f6fc;">
+    <h1>Form badword</h1>
+
+    <form action="badword.php" method="GET">
+        <label style="display:block" for="paragraph">Inserisci un testo</label>
+        <textarea name="original" id="paragrapg" cols="30" rows="10" placeholder="scrivi qualcosa..."></textarea>
+
+        <hr>
+
+        <label style="display:block" for="paragraph">Inserisci una parola da censurare</label>
+        <input id="paragraph" type="text" name="censured" placeholder="Scrivi qualcosa...">
+
+        <button>Invia</button>
+
+    </form>
 </body>
 </html>
